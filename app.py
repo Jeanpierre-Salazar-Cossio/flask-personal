e61c96cc23508c0b8b3ed87058051099e387b50c
LAB_C_REAL <labc@continental.edu.pe>
2025-02-06 21:44:51 -0500

    primer commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/contacto")
+def contacto():
+    return render_template("contacto.html")
+
+@app.route("/acerca-de-mi")
+def acerca_de():
+    return render_template("acerca-de-mi.html")
+
+if __name__ == "__main__":
+     app.run(debug=True)
